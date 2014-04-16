27f227d6716cd6ae8e5599db4309c07e958b4470
kupa25 <kupa25@live.com>
2014-04-16 17:47:03 -0400

    added calendar

---
+++ Temple/Controllers/CalendarController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Temple.Controllers
+{
+    public class CalendarController : Controller
+    {
+        //
+        // GET: /Calendar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
